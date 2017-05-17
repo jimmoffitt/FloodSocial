f147ad55bc290059cd4de226f3431754b813d120
Jim Moffitt <jmoffitt@twitter.com>
2017-05-17 12:15:49 -0600

    Getting sinatra app set up...

---
+++ enroll/app/helpers/event_manager.rb
@@ -0,0 +1,119 @@
+#POST requests to /webhooks/twitter arrive here.
+#Twitter Account Activity API send events as POST requests with DM JSON payloads.
+
+require 'json'
+#Two helper classes... 
+require_relative 'send_direct_message'
+require_relative 'power_track_rules_manager'
+
+class EventManager
+
+   attr_accessor :DMsender, :RulesManager
+
+   def initialize
+	  #puts 'Creating SendDirectMessage object'
+	  @DMSender = SendDirectMessage.new
+	  #External 'things' this object can trigger...
+	  @RulesManager = PowerTrackRulesManager.new
+   end
+
+   #responses are based on options' metadata settings.
+   #pick_from_list, select_on_map, location list items (e.g. 'location_list_choice: Austin' or 'Fort Worth')
+   #map_selection (triggers a fetch of the shared coordinates)
+
+   def handle_event(events)
+
+	  #puts "Event handler processing: #{events}"
+
+	  events = JSON.parse(events)
+
+	  if events.key? ('direct_message_events')
+
+		 dm_events = events['direct_message_events']
+
+		 dm_events.each do | dm_event |
+
+			if dm_event['type'] == 'message_create'
+
+			   #Is this a response? Test for the 'quick_reply_response' key.
+			   is_response = dm_event['message_create'] && dm_event['message_create']['message_data'] && dm_event['message_create']['message_data']['quick_reply_response']
+
+			   if is_response
+				  response = dm_event['message_create']['message_data']['quick_reply_response']['metadata']
+				  user_id = dm_event['message_create']['sender_id']
+
+				  puts "User #{user_id} answered with #{response}"
+
+				  if response == 'learn_more'
+					 @DMSender.send_system_info(user_id)
+				  elsif response == 'return_to_system'
+					 @DMSender.send_welcome_message(user_id)
+				  elsif response == 'select_on_map'
+					 @DMSender.send_map(user_id)
+				  elsif response == 'pick_from_list'
+					 @DMSender.send_location_list(user_id)
+				  elsif response.include? 'location_list_choice'
+					 location_choice = response['location_list_choice: '.length..-1]
+					 coordinates = []
+					 coordinates = @RulesManager.add_subscription_for_list_selection(user_id, location_choice)
+					 location_choice = "#{location_choice} (a 25-mile radius circle centered at #{coordinates[0]}, #{coordinates[1]} to be specific)"
+					 @DMSender.send_confirmation(user_id, location_choice)
+				  elsif response == 'map_selection'
+					 #Navigate and extract the long lat details
+
+					 #Do we have a Twitter Place or exact coordinates....?
+					 location_type = dm_event['message_create']['message_data']['attachment']['location']['type']
+
+					 if location_type == 'shared_coordinate'
+						coordinates = dm_event['message_create']['message_data']['attachment']['location']['shared_coordinate']['coordinates']['coordinates']
+						location_choice = "25-mile radius circle centered at #{coordinates[0].round(4)}, #{coordinates[1].round(4)}."
+					 else
+						coordinates = dm_event['message_create']['message_data']['attachment']['location']['shared_place']['place']['centroid']
+						place_name = dm_event['message_create']['message_data']['attachment']['location']['shared_place']['place']['name']
+						location_choice = "25-mile radius circle centered on #{place_name} (with centroid of #{coordinates[0].round(4)}, #{coordinates[1].round(4)})."
+					 end
+
+					 @RulesManager.add_subscription_for_coordinates(user_id, coordinates[0].round(4), coordinates[1].round(4))
+
+					 @DMSender.send_confirmation(user_id, location_choice)
+				  elsif response == 'list'
+					 puts "Retrieve current config for user #{user_id}. "
+					 subscriptions = @RulesManager.get_subscriptions(user_id)
+					 puts "subscriptions: #{subscriptions}"
+					 @DMSender.send_subscription_list(user_id, subscriptions)
+				  else #we have an answer to one of the above.
+					 puts "UNHANDLED user response: #{response}"
+				  end
+			   else
+				  #Since this DM is not a response to a QR, let's check for other 'action' commands
+				  puts 'Received a command/question DM? Need to track conversation stage?'
+
+				  request = dm_event['message_create']['message_data']['text']
+				  user_id = dm_event['message_create']['sender_id']
+
+				  if request.downcase == 'add'
+					 puts 'Send QR to add an area'
+					 #Send QR for which 'select area' method
+					 @DMSender.send_welcome_message(user_id)
+
+				  elsif request.downcase == 'unsubscribe' or request.downcase == 'quit' or request.downcase == 'stop'
+					 puts 'unsubscribe'
+					 @RulesManager.delete_subscription(user_id)
+					 @DMSender.send_unsubscribe(user_id)
+
+				  elsif request.downcase == 'list'
+					 puts "Retrieve current config for user #{user_id}. "
+					 subscriptions = @RulesManager.get_subscriptions(user_id)
+					 puts "subscriptions: #{subscriptions}"
+					 @DMSender.send_subscription_list(user_id, subscriptions)
+				  end
+			   end
+			else
+			   puts "Hey a new, unhandled type has been implemented on the Twitter side."
+			end
+		 end
+	  else
+		 puts "Received test JSON."
+	  end
+   end
+ end
